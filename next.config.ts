5fc615454a826bfabdd6f6437eda8efafbc822f8
Kourosh Torabijafroudi <kooroshtorabi@gmail.com>
2025-11-15 01:20:01 +0100

    next

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
-import type { NextConfig } from "next";
+const { i18n } = require("./next-i18next.config");
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
 
-  i18n: {
-    locales: ["fa", "en", "de"],
-    defaultLocale: "en",
-    localeDetection: false,
-  },
+//   i18n: {
+//     locales: ["fa", "en", "de"],
+//     defaultLocale: "en",
+//     localeDetection: false,
+//   },
+// };
+
+// export default nextConfig;
+module.exports = {
+  i18n,
 };
-
-export default nextConfig;
