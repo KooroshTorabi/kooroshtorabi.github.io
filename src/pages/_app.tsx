3c9d73db35cbf79a5949bfc253c7b7a48e8c7151
Kourosh Torabijafroudi <kooroshtorabi@gmail.com>
2025-11-16 01:33:58 +0100

    fix: Resolve Vercel 500 error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
-// src/pages/_app.tsx
-
 import "@styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
-import nextI18NextConfig from "../../next-i18next.config"; // مسیر دهی صحیح
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default appWithTranslation(MyApp, nextI18NextConfig);
+export default appWithTranslation(MyApp);
