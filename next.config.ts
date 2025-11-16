eea7dad662595e1267d0d9a5cea8cd341d8d1dd1
Kourosh Torabijafroudi <kooroshtorabi@gmail.com>
2025-11-16 02:31:30 +0100

    change next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,23 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  i18n: {
-    locales: ["fa", "en", "de"],
-    defaultLocale: "en",
-    localeDetection: false,
-  },
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   i18n: {
+//     locales: ["fa", "en", "de"],
+//     defaultLocale: "en",
+//     localeDetection: false,
+//   },
+// };
+
+// export default nextConfig;
+
+// next.config.js
+const { i18n } = require("./next-i18next.config"); // اطمینان از فراخوانی فایل
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... سایر تنظیمات Next.js
+  i18n, // اضافه کردن پیکربندی i18n
 };
 
-export default nextConfig;
+module.exports = nextConfig;
